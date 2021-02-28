503f40d2e8938efd5496bab08b2e076290e9db90
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-02-28 15:41:35 +0100

    Fix error in KernelSVMEstimator

--- estimators/KernelSVMEstimator.py
+++ estimators/KernelSVMEstimator.py
@@ -1,12 +1,15 @@
 """Implement the KernelSVMEstimator class."""
 import numpy as np
 import cvxpy as cp
+import copy
+
+from .BaseEstimator import BaseEstimator
 
 
-class KernelSVMEstimator():
+class KernelSVMEstimator(BaseEstimator):
     """Implement a sklearn compatible kernel SVM estimator."""
 
-    def __init__(self, lbd, Kernel, kernel_params):
+    def __init__(self, lbd, kernel, kernel_params):
         """Init.
 
         Parameters:
@@ -14,28 +17,52 @@ class KernelSVMEstimator():
             lbd : float
                 Weight in the optimization problem.
             Kernel : class inheriting from Kernel
-                The kernel.
+                The kernel. Note that the class must not be instanciated.
             kernel_params : dict
                 The parameters given to the Kernel class when instanciating.
 
         """
-        self.kernel = Kernel(**kernel_params)
+        self.kernel = kernel(**kernel_params)
         self.lbd = lbd
 
         self.X = None
         self.alpha = None
+        self.labels = None
+
+    def _transform_labels(self, y):
+        """Transform labels into {-1, 1}."""
+        labels = np.unique(y)
+        if len(labels) > 2:
+            raise ValueError(f'Only two classes are accepted in y, '
+                             f'found: {labels}')
+
+        self.labels = labels
+
+        y = y.copy()
+        y[y == labels[0]] = -1
+        y[y == labels[1]] = 1
+
+        return y
+
+    def _inverse_transform_labels(self, y):
+        """Transform labels from {-1, 1} to original ones."""
+        y = y.copy()
+        neg_idx = y < 0
+        y[neg_idx] = self.labels[0]
+        y[~neg_idx] = self.labels[1]
+
+        return y
 
     def fit(self, X, y):
         K = self.kernel(X, X)
-
         n = K.shape[0]
         e = cp.Variable(n)
         alpha = cp.Variable(n)
         ones = np.ones((n, 1))
 
         # Set labels in {-1, 1}
+        y = self._transform_labels(y)
         y = np.array(y)
-        y[y == 0] = -1
 
         objective = cp.Minimize(1/n*ones.T@e + self.lbd*cp.quad_form(alpha, K))
         constraints = [e >= 0, y*K@alpha + e - 1 >= 0]
@@ -51,4 +78,5 @@ class KernelSVMEstimator():
     def predict(self, X):
         K_test = self.kernel(self.X, X)
         y_pred = self.alpha@K_test
+        y_pred = self._inverse_transform_labels(y_pred)
         return y_pred
---
+++ estimators/__init__.py
@@ -0,0 +1,1 @@
+from .KernelSVMEstimator import KernelSVMEstimator
