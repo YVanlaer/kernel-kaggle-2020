701fba00a84ffd5d1efb9a6974dc22e41ccf6c2b
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-02-28 16:30:24 +0100

    Typo

--- kernels/BaseKernel.py
+++ kernels/BaseKernel.py
@@ -5,8 +5,6 @@ from abc import ABC, abstractmethod
 class BaseKernel(ABC):
     """Abstract class for creating kernels."""
 
-    # def __init__(self):
-
     @abstractmethod
     def __call__(self, X1, X2):
         """Create a kernel matrix given inputs."""
