6b77c189a63263ff605a1cdff11fc2ef53b8329b
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-03-01 19:17:31 +0100

    Move __repr__ to BaseKernel

--- kernels/BaseKernel.py
+++ kernels/BaseKernel.py
@@ -9,3 +9,7 @@ class BaseKernel(ABC):
     def __call__(self, X1, X2):
         """Create a kernel matrix given inputs."""
         pass
+
+    def __repr__(self):
+        d = ', '.join([f'{k}={v}' for k, v in self.__dict__.items()])
+        return f'{self.__class__.__name__}({d})'
