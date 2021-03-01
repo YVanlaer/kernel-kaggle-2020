90e576e2657e524c2f6b3ad01492ab261e9889d7
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-03-01 16:26:23 +0100

    Store precomputed DTW matrices

--- kernels/DTWKernel.py
+++ kernels/DTWKernel.py
@@ -1,13 +1,17 @@
 """Implement the GaussianKernel class."""
 import numpy as np
+import pandas as pd
 from scipy.spatial import distance
 from dtaidistance import dtw
+import hashlib
 
 from .BaseKernel import BaseKernel
 
 
 class DTWKernel(BaseKernel):
     """Implement a kernel seeing sequences as time series and dist as DTW."""
+    # Store precomputed DTW matrices to save time
+    dtw_matrices: dict = {}
 
     def __init__(self, var=1):
         """Init.
@@ -46,19 +50,46 @@ class DTWKernel(BaseKernel):
     def sequence_to_timeseries(sequences):
         return np.array([DTWKernel.one_sequence_to_timeseries(s) for s in sequences])
 
+    @staticmethod
+    def hash_sequences(Xs):
+        X = pd.concat(Xs, axis=0)
+        return hashlib.sha256(pd.util.hash_pandas_object(X).values).hexdigest()
+
+    def register_dtw_matrix(self, matrix, X1, X2):
+        h = self.hash_sequences([X1, X2])
+        print(f'Registering DTW matrix for hash {h}')
+        self.dtw_matrices[h] = matrix
+
+    def get_dtw_matrix(self, X1, X2):
+        h = self.hash_sequences([X1, X2])
+        print(f'Retrieving DTW matrix for hash {h}', end='\t')
+        return self.dtw_matrices.get(h, None)
+
     def __call__(self, X1, X2):
-        X1 = self.sequence_to_timeseries(X1)
-        X2 = self.sequence_to_timeseries(X2)
-        n1 = X1.shape[0]
-        n2 = X2.shape[0]
+        print(f'Keys in dtw matrices: {len(self.dtw_matrices.keys())}')
+        # Check if kernel matrix has already been computed
+        dtw_matrix = self.get_dtw_matrix(X1, X2)
 
-        X = np.concatenate([X1, X2], axis=0)
+        if dtw_matrix is None:
+            print('DTW matrix not found')
 
-        dist = dtw.distance_matrix_fast(X, parallel=True,
-                                        block=((0, n1), (n1, n1+n2)))
-        dist = dist[:n1, n1:]
+            X1_ = self.sequence_to_timeseries(X1)
+            X2_ = self.sequence_to_timeseries(X2)
+            n1 = X1_.shape[0]
+            n2 = X2_.shape[0]
 
-        return np.exp(-np.power(dist, 2)/(2*self.var))
+            X = np.concatenate([X1_, X2_], axis=0)
+
+            dtw_matrix = dtw.distance_matrix_fast(X, parallel=True,
+                                                  block=((0, n1), (n1, n1+n2)))
+            dtw_matrix = dtw_matrix[:n1, n1:]
+
+            self.register_dtw_matrix(dtw_matrix, X1, X2)
+        else:
+            print('DTW matrix found')
+
+        K = np.exp(-np.power(dtw_matrix, 2)/(2*self.var))
+        return K
 
     def __repr__(self):
         return f'DTWKernel({self.var})'
