39dfa656fddd2fe17861a6871118772d8c50ce4a
Thomas Chabal <t.chabal@hotmail.fr>
2021-03-18 01:31:33 +0100

    Implement Local alignment kernel

--- kernels/__init__.py
+++ kernels/__init__.py
@@ -2,3 +2,4 @@ from .GaussianKernel import GaussianKernel
 from .DTWKernel import DTWKernel
 from .SpectrumKernel import SpectrumKernel
 from .MismatchKernel import MismatchKernel
+from .LocalAlignmentKernel import LocalAlignmentKernel
